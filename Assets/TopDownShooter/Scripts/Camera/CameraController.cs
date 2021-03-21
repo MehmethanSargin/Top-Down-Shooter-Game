7ffbe5852451aa49222cea191dadbeaae896645e
Mehmethan Sargin <mehmethansargin123@gmail.com>
2021-03-22 02:28:25 +0300

    feat(camera): add camera controller # add camera controller for follow the player.

---
+++ Assets/TopDownShooter/Scripts/Camera/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopDownShooter
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField]
+        private CameraSettings cameraSettings;
+        [SerializeField]
+        private Transform targetTransform;
+        [SerializeField] 
+        private Transform cameraTransform;
+
+        private void Update()
+        {
+            CameraMovementFollow();
+            CameraRotationFollow();
+        }
+
+        private void CameraRotationFollow()
+        {
+            cameraTransform.rotation = Quaternion.Lerp(cameraTransform.rotation, Quaternion.LookRotation
+            (targetTransform.position - cameraTransform.position), Time.deltaTime * cameraSettings.RotationLerpSpeed);
+        }
+
+        private void CameraMovementFollow()
+        {
+            cameraTransform.position = Vector3.Lerp(cameraTransform.position, targetTransform.position +
+            cameraSettings.PositionOffset, Time.deltaTime * cameraSettings.PositionLerp);
+        }
+    }
+}
+
