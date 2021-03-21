f0bc681427dd47dfa38d7971b5eb1be512864904
Mehmethan Sargin <mehmethansargin123@gmail.com>
2021-03-22 02:26:39 +0300

    feat(settings): add camera settings #add camera settings to create a scriptable object

---
+++ Assets/TopDownShooter/Scripts/Camera/CameraSettings.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopDownShooter
+{
+    [CreateAssetMenu(menuName ="TopDownShooter/Camera/Camera Settings")]
+    public class CameraSettings : ScriptableObject
+    {
+        [Header("Rotation")]
+        [SerializeField] 
+        private float rotationLerpSpeed = 1;
+        public float RotationLerpSpeed 
+        { 
+            get 
+            { 
+                return rotationLerpSpeed; 
+            } 
+        }
+
+        [Header("Position")]
+        [SerializeField] 
+        private Vector3 positionOffset;
+        public Vector3 PositionOffset 
+        {
+            get 
+            { 
+                return positionOffset; 
+            } 
+        }
+
+        [SerializeField] 
+        private float positionLerp = 1;
+        public float PositionLerp 
+        {
+            get 
+            { return positionLerp; 
+            } 
+        }
+    }
+}
+
