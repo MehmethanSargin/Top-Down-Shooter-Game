c2927bf0d7962f18ac72ed045c2abcecb06f577d
Mehmethan Sargin <mehmethansargin123@gmail.com>
2021-03-29 20:48:02 +0300

    feat(player): add basic input system for player #.

---
+++ Assets/TopDownShooter/Scripts/Input/InputData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopDownShooter.PlayerInput
+{
+    [CreateAssetMenu(menuName = "TopDownShooter/Input/Input Data")]
+    public class InputData : ScriptableObject
+    {
+        public float Horizontal;
+        public float Vertical;
+    }
+}
+
---
+++ Assets/TopDownShooter/Scripts/Input/InputManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopDownShooter.PlayerInput
+{
+    public class InputManager : MonoBehaviour
+    {
+        [SerializeField]
+        private InputData inputData;
+        void Update()
+        {
+            inputData.Horizontal = Input.GetAxis("Horizontal");
+            inputData.Vertical = Input.GetAxis("Vertical");
+        }
+    }
+
+}
