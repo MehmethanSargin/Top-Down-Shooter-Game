87778674514d10bacc2261b222989c183df8169c
Mehmethan Sargin <mehmethansargin123@gmail.com>
2021-03-29 20:49:36 +0300

    style(camera):edit spaces #

--- Assets/TopDownShooter/Scripts/Camera/CameraSettings.cs
+++ Assets/TopDownShooter/Scripts/Camera/CameraSettings.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace TopDownShooter
+namespace TopDownShooter.Camera
 {
     [CreateAssetMenu(menuName ="TopDownShooter/Camera/Camera Settings")]
     public class CameraSettings : ScriptableObject
@@ -10,33 +10,16 @@ namespace TopDownShooter
         [Header("Rotation")]
         [SerializeField] 
         private float rotationLerpSpeed = 1;
-        public float RotationLerpSpeed 
-        { 
-            get 
-            { 
-                return rotationLerpSpeed; 
-            } 
-        }
+        public float RotationLerpSpeed { get { return rotationLerpSpeed;} }
 
         [Header("Position")]
         [SerializeField] 
         private Vector3 positionOffset;
-        public Vector3 PositionOffset 
-        {
-            get 
-            { 
-                return positionOffset; 
-            } 
-        }
+        public Vector3 PositionOffset { get {  return positionOffset;} }
 
         [SerializeField] 
         private float positionLerp = 1;
-        public float PositionLerp 
-        {
-            get 
-            { return positionLerp; 
-            } 
-        }
+        public float PositionLerp  { get { return positionLerp;} }
     }
 }
 
