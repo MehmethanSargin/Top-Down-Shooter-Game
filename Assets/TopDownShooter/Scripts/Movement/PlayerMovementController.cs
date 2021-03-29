4b5f906806951719eab12939ff3b763dc9df9278
Mehmethan Sargin <mehmethansargin123@gmail.com>
2021-03-29 20:50:06 +0300

    feat(player): add player movement system basic #.

---
+++ Assets/TopDownShooter/Scripts/Movement/PlayerMovementController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TopDownShooter.PlayerInput;
+using UnityEngine;
+
+namespace TopDownShooter.PlayerMovement
+{
+    public class PlayerMovementController : MonoBehaviour
+    {
+        [SerializeField]
+        private InputData inputData;
+
+        [SerializeField]
+        private Rigidbody body;
+
+        [SerializeField]
+        private PlayerMovementSettings playerMovementSettings;
+
+        private void Update()
+        {
+            body.MovePosition(body.position + (body.transform.forward * inputData.Vertical * playerMovementSettings.VerticalSpeed ));
+            
+            body.MovePosition(body.position + (body.transform.right * inputData.Horizontal * playerMovementSettings.HorizontalSpeed));
+        }
+    }
+
+}
